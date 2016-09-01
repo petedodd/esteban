3e816a46c34fcd366627d2a7d3e8562cd35836da
sabidius <pete.dodd24@gmail.com>
2016-09-01 13:57:17 +0100

    data added, vignette started

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@
   packageStartupMessage("Welcome to esteban!\nAn experimental package for using transmission models to estimate TB burden.\n(c) P.J. Dodd 2016: released under CC BY-SA 4.0 licence (see licence or https://creativecommons.org/licenses/by-sa/4.0/).\nSee GitHub REAME and vignette for more details on use (https://github.com/petedodd/esteban).")
 }
 
-## This is almost certainlty not best practice and would be hood to do away with
+## This is almost certainlty not best practice and would be good to do away with
 .onLoad <- function(libname='esteban',pkgname='esteban'){
+    data(rngs)                          #load up
 }
