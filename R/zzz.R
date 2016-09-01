96f3b50082606ce6d8604dd610b7b1949a484241
sabidius <pete.dodd24@gmail.com>
2016-09-01 14:03:56 +0100

    load data moved

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,9 @@
 .onAttach <- function(libname='esteban',pkgname='esteban'){
-  packageStartupMessage("Welcome to esteban!\nAn experimental package for using transmission models to estimate TB burden.\n(c) P.J. Dodd 2016: released under CC BY-SA 4.0 licence (see licence or https://creativecommons.org/licenses/by-sa/4.0/).\nSee GitHub REAME and vignette for more details on use (https://github.com/petedodd/esteban).")
+    packageStartupMessage("Welcome to esteban!\nAn experimental package for using transmission models to estimate TB burden.\n(c) P.J. Dodd 2016: released under CC BY-SA 4.0 licence (see licence or https://creativecommons.org/licenses/by-sa/4.0/).\nSee GitHub REAME and vignette for more details on use (https://github.com/petedodd/esteban).")
+    data(rngs)                          #load up
 }
 
 ## This is almost certainlty not best practice and would be good to do away with
 .onLoad <- function(libname='esteban',pkgname='esteban'){
-    data(rngs)                          #load up
+
 }
